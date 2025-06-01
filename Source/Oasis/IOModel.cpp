ba730ec648d228cdf061cdbb1db33885158b40b3
Emil Keshishian <42214323+EmilKD@users.noreply.github.com>
2025-06-01 18:39:51 +0430

    feat: const references used for efficiency and a little bit of progress on fixing the vertex texture coordinates.

--- Source/Oasis/IOModel.cpp
+++ Source/Oasis/IOModel.cpp
@@ -7,54 +7,72 @@ using model::Model, std::string;
 
 namespace IOModel{
 
-	model::Model LoadObjModel(std::string filePath) {
+	Model LoadObjModel(string filePath) {
 
 		string fileAsText;
 		std::ifstream objFile(filePath);
 		Model tempModel;
+		std::vector<float> tempBuffer;
 
-		while (std::getline(objFile, fileAsText)) {
+		while (getline(objFile, fileAsText)) {
 
 
 			if (fileAsText.substr(0, fileAsText.find(" ") + 1) == "v ") {
-				size_t xpos1 = fileAsText.find(" ") + 1;
-				size_t xpos2 = fileAsText.substr(xpos1).find(" ") + xpos1;
-				size_t ypos1 = xpos2 + 1;
-				size_t ypos2 = fileAsText.substr(ypos1).find(" ") + ypos1;
-				size_t zpos1 = ypos2 + 1;
+				const int numOfParams = std::count(fileAsText.begin(), fileAsText.end(), ' ');
 
-				//std::cout << xpos1 << " " << xpos2 << " " << ypos1 << " " << ypos2 << " " << zpos1 << std::endl;
+				rsize_t pos1 = fileAsText.find(" ") + 1;
+				for (size_t i = 0; i < numOfParams; i++)
+				{
+					const size_t& pos2 = fileAsText.substr(pos1).find(" ") + pos1;
 
-				//std::cout << fileAsText.substr(xpos1, xpos2-xpos1) << ", " << fileAsText.substr(ypos1, ypos2-ypos1) << ", " << fileAsText.substr(zpos1) << std::endl;
-				tempModel.vertexPoses.push_back(std::stof(fileAsText.substr(xpos1, xpos2 - xpos1)));
-				tempModel.vertexPoses.push_back(std::stof(fileAsText.substr(ypos1, ypos2 - ypos1)));
-				tempModel.vertexPoses.push_back(std::stof(fileAsText.substr(zpos1)));
-
-
+					tempModel.vertexPoses.push_back(std::stof(fileAsText.substr(pos1, pos2 - pos1)));
+					pos1 = pos2 + 1;
+				}
 			}
 			else if (fileAsText.substr(0, fileAsText.find(" ") + 1) == "vt ") {
-				size_t upos1 = fileAsText.find(" ") + 1;
-				size_t upos2 = fileAsText.substr(upos1).find(" ") + upos1;
-				size_t vpos1 = upos2 + 1;
+				const int& numOfParams = std::count(fileAsText.begin(), fileAsText.end(), ' ');
 
-				tempModel.vertexTexCoords.push_back(std::stof(fileAsText.substr(upos1, upos2 - upos1)));
-				tempModel.vertexTexCoords.push_back(std::stof(fileAsText.substr(vpos1)));
+				size_t pos1 = fileAsText.find(" ") + 1;
+				for (size_t i = 0; i < numOfParams; i++)
+				{
+					const size_t& pos2 = fileAsText.substr(pos1).find(" ") + pos1;
+
+					tempModel.vertexTexCoords.push_back(std::stof(fileAsText.substr(pos1, pos2 - pos1)));
+					pos1 = pos2 + 1;
+				}
+			}
+			else if (fileAsText.substr(0, fileAsText.find(" ") + 1) == "vn ") {
+				const int& numOfParams = std::count(fileAsText.begin(), fileAsText.end(), ' ');
+
+				size_t pos1 = fileAsText.find(" ") + 1;
+				for (size_t i = 0; i < numOfParams; i++)
+				{
+					const size_t& pos2 = fileAsText.substr(pos1).find(" ") + pos1;
+
+					tempModel.vertexNormals.push_back(std::stof(fileAsText.substr(pos1, pos2 - pos1)));
+					pos1 = pos2 + 1;
+				}
 			}
 			else if (fileAsText.substr(0, fileAsText.find(" ") + 1) == "f ") {
-				size_t id1pos1 = fileAsText.find(" ") + 1;
-				size_t id1pos2 = fileAsText.find("/");
-				size_t id2pos1 = fileAsText.substr(id1pos2).find(" ") + id1pos2 + 1;
-				size_t id2pos2 = fileAsText.substr(id2pos1).find("/") + id2pos1;
-				size_t id3pos1 = fileAsText.substr(id2pos2).find(" ") + id2pos2 + 1;
-				size_t id3pos2 = fileAsText.substr(id3pos1).find("/") + id3pos1;
+				const int& numOfParams = std::count(fileAsText.begin(), fileAsText.end(), ' ');
 
-				// Debug print
-				// std::cout << fileAsText << std::endl;
-				// std::cout << fileAsText.substr(id1pos1, id1pos2 - id1pos1) << ", " << fileAsText.substr(id2pos1, id2pos2 - id2pos1) << ", " << fileAsText.substr(id3pos1, id3pos2 - id3pos1) << std::endl;
+				size_t pos1 = fileAsText.find(" ") + 1;
+				for (size_t i = 0; i < numOfParams; i++)
+				{
+					const size_t& pos2 = fileAsText.substr(pos1).find(" ") + pos1;
+					const size_t& vtpos = fileAsText.substr(pos1).find("/") + pos1 + 1;
+					const size_t& vnpos = fileAsText.substr(vtpos).find("/") + vtpos + 1;
 
-				tempModel.indices.push_back(std::stoi(fileAsText.substr(id1pos1, id1pos2 - id1pos1)));
-				tempModel.indices.push_back(std::stoi(fileAsText.substr(id2pos1, id2pos2 - id2pos1)));
-				tempModel.indices.push_back(std::stoi(fileAsText.substr(id3pos1, id3pos2 - id3pos1)));
+					const int& vertexIndex = std::stoi(fileAsText.substr(pos1, pos2 - pos1)) - 1;
+					tempModel.indices.push_back(vertexIndex);
+					/*tempBuffer.insert(tempBuffer.begin() + 5 * vertexIndex, tempModel.vertexPoses.at(3 * vertexIndex));
+					tempBuffer.insert(tempBuffer.begin() + 5 * vertexIndex + 1, tempModel.vertexPoses.at(3 * vertexIndex + 1));
+					tempBuffer.insert(tempBuffer.begin() + 5 * vertexIndex + 2, tempModel.vertexPoses.at(3 * vertexIndex + 2));
+					tempBuffer.insert(tempBuffer.begin() + 5 * vertexIndex + 3, tempModel.vertexTexCoords.at(2 * std::stoi(fileAsText.substr(vtpos, vnpos - vtpos - 1)) - 2));
+					tempBuffer.insert(tempBuffer.begin() + 5 * vertexIndex + 4, tempModel.vertexTexCoords.at(2 * std::stoi(fileAsText.substr(vtpos, vnpos - vtpos - 1)) - 1));*/
+
+					pos1 = pos2 + 1;
+				}
 			}
 		}
 
@@ -63,6 +81,7 @@ namespace IOModel{
 		std::cout << "number of indices: " << tempModel.indices.size() << std::endl;
 		std::cout << "number of vertex texture coords: " << tempModel.vertexTexCoords.size() / 2 << std::endl;
 		tempModel.CreateDataBuffer();
+		//tempModel.vertexBuffer = tempBuffer;
 		return tempModel;
 	}
 
