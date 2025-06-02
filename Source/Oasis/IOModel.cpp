f44f87c14b59cc5178ee127ac56495ad397681bf
Emil Keshishian <42214323+EmilKD@users.noreply.github.com>
2025-06-03 01:18:43 +0330

    feat: vertex texture coordinate importing fixed.

--- Source/Oasis/IOModel.cpp
+++ Source/Oasis/IOModel.cpp
@@ -1,6 +1,8 @@
 #pragma once
 #include<iostream>
 #include<fstream>
+#include<map>
+#include<tuple>
 #include"IOModel.h"
 
 using model::Model, std::string;
@@ -9,13 +11,25 @@ namespace IOModel{
 
 	Model LoadObjModel(string filePath) {
 
-		string fileAsText;
-		std::ifstream objFile(filePath);
 		Model tempModel;
 		std::vector<float> tempBuffer;
+		std::map<std::tuple<int, int>, int> indexMap;
+		string fileAsText;
+		std::ifstream file;
+		file.open(filePath);
 
-		while (getline(objFile, fileAsText)) {
+		if (!file.is_open())
+		{
+			std::cerr << "Error openning file: " << filePath;
 
+			if (file.bad()) {
+				std::cerr << "Fatal error: badbit is set." << std::endl;
+			}
+
+			return tempModel;
+		}
+
+		while (getline(file, fileAsText)) {
 
 			if (fileAsText.substr(0, fileAsText.find(" ") + 1) == "v ") {
 				const int numOfParams = std::count(fileAsText.begin(), fileAsText.end(), ' ');
@@ -29,6 +43,7 @@ namespace IOModel{
 					pos1 = pos2 + 1;
 				}
 			}
+
 			else if (fileAsText.substr(0, fileAsText.find(" ") + 1) == "vt ") {
 				const int& numOfParams = std::count(fileAsText.begin(), fileAsText.end(), ' ');
 
@@ -41,6 +56,7 @@ namespace IOModel{
 					pos1 = pos2 + 1;
 				}
 			}
+
 			else if (fileAsText.substr(0, fileAsText.find(" ") + 1) == "vn ") {
 				const int& numOfParams = std::count(fileAsText.begin(), fileAsText.end(), ' ');
 
@@ -53,6 +69,7 @@ namespace IOModel{
 					pos1 = pos2 + 1;
 				}
 			}
+
 			else if (fileAsText.substr(0, fileAsText.find(" ") + 1) == "f ") {
 				const int& numOfParams = std::count(fileAsText.begin(), fileAsText.end(), ' ');
 
@@ -62,15 +79,22 @@ namespace IOModel{
 					const size_t& pos2 = fileAsText.substr(pos1).find(" ") + pos1;
 					const size_t& vtpos = fileAsText.substr(pos1).find("/") + pos1 + 1;
 					const size_t& vnpos = fileAsText.substr(vtpos).find("/") + vtpos + 1;
+					const size_t& vtid = std::stoi(fileAsText.substr(vtpos, vnpos - vtpos - 1)) - 1;
+					const int& vid = std::stoi(fileAsText.substr(pos1, pos2 - pos1)) - 1;
+					const std::tuple<int, int>& key{ vid, vtid };
 
-					const int& vertexIndex = std::stoi(fileAsText.substr(pos1, pos2 - pos1)) - 1;
-					tempModel.indices.push_back(vertexIndex);
-					/*tempBuffer.insert(tempBuffer.begin() + 5 * vertexIndex, tempModel.vertexPoses.at(3 * vertexIndex));
-					tempBuffer.insert(tempBuffer.begin() + 5 * vertexIndex + 1, tempModel.vertexPoses.at(3 * vertexIndex + 1));
-					tempBuffer.insert(tempBuffer.begin() + 5 * vertexIndex + 2, tempModel.vertexPoses.at(3 * vertexIndex + 2));
-					tempBuffer.insert(tempBuffer.begin() + 5 * vertexIndex + 3, tempModel.vertexTexCoords.at(2 * std::stoi(fileAsText.substr(vtpos, vnpos - vtpos - 1)) - 2));
-					tempBuffer.insert(tempBuffer.begin() + 5 * vertexIndex + 4, tempModel.vertexTexCoords.at(2 * std::stoi(fileAsText.substr(vtpos, vnpos - vtpos - 1)) - 1));*/
 
+					if (!indexMap.contains(key))
+					{
+						indexMap[key] = tempBuffer.size() / 5;
+						tempBuffer.push_back(tempModel.vertexPoses.at(3 * vid));
+						tempBuffer.push_back(tempModel.vertexPoses.at(3 * vid + 1));
+						tempBuffer.push_back(tempModel.vertexPoses.at(3 * vid + 2));
+						tempBuffer.push_back(tempModel.vertexTexCoords.at(2 * vtid));
+						tempBuffer.push_back(tempModel.vertexTexCoords.at(2 * vtid + 1));
+					}
+
+					tempModel.indices.push_back(indexMap[key]);
 					pos1 = pos2 + 1;
 				}
 			}
@@ -80,9 +104,7 @@ namespace IOModel{
 		std::cout << "number of vertices: " << tempModel.vertexPoses.size() / 3 << std::endl;
 		std::cout << "number of indices: " << tempModel.indices.size() << std::endl;
 		std::cout << "number of vertex texture coords: " << tempModel.vertexTexCoords.size() / 2 << std::endl;
-		tempModel.CreateDataBuffer();
-		//tempModel.vertexBuffer = tempBuffer;
+		tempModel.vertexBuffer = tempBuffer;
 		return tempModel;
 	}
-
 }
--- Source/Oasis/Model.cpp
+++ Source/Oasis/Model.cpp
@@ -5,18 +5,6 @@
 
 namespace model
 {
-	void Model::CreateDataBuffer() {
-		for (size_t id = 0; id < this->vertexPoses.size() / 3; id++) {
-			vertexBuffer.push_back(vertexPoses.at(3 * id));
-			vertexBuffer.push_back(vertexPoses.at(3 * id + 1));
-			vertexBuffer.push_back(vertexPoses.at(3 * id + 2));
-			vertexBuffer.push_back(vertexTexCoords.at(2 * id));
-			vertexBuffer.push_back(vertexTexCoords.at(2 * id + 1));
-		}
-	}
-
-	using std::string_view, std::string, std::ifstream, std::getline;
-
 	// Cube Coordinates
 	std::vector<float> Box = {
 	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
@@ -61,4 +49,11 @@ namespace model
 	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
+
+	/*std::vector plane = {
+	0.f, -0.5f, 0.5f, 0.f, 1.f,
+	0.f, 0.5f, 0.5f, 1.f, 1.f,
+	0.f, -0.f, -0.f, 0.f, 0.f,
+	0.f, 0.5f, -0.5f, 1.f, 0.f
+	};*/
 }
