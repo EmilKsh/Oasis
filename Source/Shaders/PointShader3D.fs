074af920c09650feba10ab7ac45da5ced9a92ea2
Emil <emil.ke.200@gmail.com>
2026-06-25 19:58:22 +0200

    feat: point cloud fragment shader now renders circles for each point

--- Source/Shaders/PointShader3D.fs
+++ Source/Shaders/PointShader3D.fs
@@ -5,5 +5,9 @@ in vec3 pointColor;
 
 void main()
 {
-	FragColor = vec4(pointColor, 1.0);
+	vec2 p = gl_PointCoord * 2.0f - 1.0f;
+	if (dot(p,p) > 1)
+		discard;
+
+	FragColor = vec4(pointColor, 1.0f);
 }
